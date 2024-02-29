e4b70ea6a3a975e4245bf4cddac9fa1896896d84
Jiang Tianwen <twjiang@nuvoton.com>
2024-02-29 01:25:51 -0500

    autologin without typing user name or password

---
+++ package/autologin/autologin.mk
@@ -0,0 +1,21 @@
+AUTOLOGIN_VERSION = 0.1
+AUTOLOGIN_SOURCE =
+AUTOLOGIN_SITE =
+AUTOLOGIN_LICENSE = BSD-3-Clause
+AUTOLOGIN_FILES = COPYING
+
+define AUTOLOGIN_INSTALL_TARGET_CMDS
+
+	@echo "=====> autologin without typing a user name or password <====="
+
+	@if grep -Eq "^BR2_PACKAGE_AUTOLOGIN_WITHOUT_PASSWORD=y$\" ${BR2_CONFIG}; then \
+		echo "auto login without typing a user name or password."; \
+		sed -i 's|^.*respawn:/sbin/getty.*|console::respawn:-/bin/sh|' ${TARGET_DIR}/etc/inittab; \
+	else \
+		cp -f $(TOPDIR)/package/busybox/inittab ${TARGET_DIR}/etc/inittab; \
+	fi
+
+endef
+
+$(eval $(generic-package))
+
